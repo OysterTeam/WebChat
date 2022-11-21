23f1e7790d0db64f09e0afa6193d16d78c64e6e8
BacklitCat <34826904+BacklitCat@users.noreply.github.com>
2022-11-21 15:48:35 +0800

    add IsEmailValid

--- server/util.go
+++ server/util.go
@@ -3,6 +3,9 @@ package server
 import (
 	"bytes"
 	"encoding/binary"
+	"net"
+	"regexp"
+	"strings"
 )
 
 func IntToBytes(n int) []byte {
@@ -10,4 +13,22 @@ func IntToBytes(n int) []byte {
 	bytesBuffer := bytes.NewBuffer([]byte{})
 	_ = binary.Write(bytesBuffer, binary.BigEndian, x)
 	return bytesBuffer.Bytes()
+}
+
+var emailRegex = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+// IsEmailValid 检查邮件地址是否合法
+func IsEmailValid(e *string) bool {
+	if len(*e) < 3 && len(*e) > 254 {
+		return false
+	}
+	if !emailRegex.MatchString(*e) {
+		return false
+	}
+	parts := strings.Split(*e, "@")
+	mx, err := net.LookupMX(parts[1])
+	if err != nil || len(mx) == 0 {
+		return false
+	}
+	return true
 }
